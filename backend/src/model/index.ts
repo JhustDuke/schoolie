56e075384cc83f49a800573bd617ad5c2894b921
Dhuke Chike <dukelchike31@gmail.com>
2025-06-25 02:08:07 +0100

    patch route working updateModel, updateRoute

--- backend/src/model/index.ts
+++ backend/src/model/index.ts
@@ -1,6 +1,12 @@
 export * from "./createDBModel";
 export * from "./sessionYearTableModel";
+
+//get
 export * from "./getAllSessionModel";
 
+//post
 export * from "./addPupilModel";
 export * from "./getPupilByClassModel";
+
+//patch
+export * from "./updatePupilModel";
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { getRoutes } from "./allGetRoute";
 import { postRoutes } from "./allPostRoute";
+import {updatePupilData}from './updateHandler'
 import { notFoundRoutes } from "./notFound";
 
-export const allRoutes = [...getRoutes, ...postRoutes, ...notFoundRoutes];
+export const allRoutes = [...getRoutes, ...postRoutes, ...notFoundRoutes,...updatePupilData];
